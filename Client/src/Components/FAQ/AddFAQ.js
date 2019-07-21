d0d18085c191ee53d9adc8c05b18c62caf24d1a2
Hem Singh <hemsingh@publicisgroupe.net>
2019-07-21 13:52:04 +0530

    Add FAQ Form Validation

--- Client/src/Components/FAQ/AddFAQ.js
+++ Client/src/Components/FAQ/AddFAQ.js
@@ -1,31 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class AddFAQ extends React.Component {
+class AddFAQ extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            question: '',
+            answer: '',
+            isActive: true,
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        if (name === 'isActive') {
+            this.setState({ [name]: e.target.checked });
+        }
+        else {
+            this.setState({ [name]: value });
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState({ submitted: true });
+        const { question, answer, isActive } = this.state;
+        console.log(isActive);
+        // const { dispatch } = this.props;
+        // if (username && password) {
+        //     dispatch(userActions.login(username, password));
+        // }
     }
 
     render() {
+        const { loggingIn } = this.props;
+        const { question, answer, isActive, submitted } = this.state;
         return (
             <>
-                <form>
+                <form name="form" onSubmit={this.handleSubmit}>
                     <div className="modal-body">
-                        <div className="form-group">
+                        <div className={'form-group' + (submitted && !question ? ' has-error' : '')}>
                             <label htmlFor="question">Question</label>
-                            <input type="type" className="form-control" name="question" placeholder="Question" />
+                            <input type="type" className="form-control" name="question" placeholder="Question"
+                                value={question} onChange={this.handleChange}
+                            />
+                            {submitted && !question &&
+                                <div className="help-block">Question is required.</div>
+                            }
                         </div>
-                        <div className="form-group">
+                        <div className={'form-group' + (submitted && !answer ? ' has-error' : '')}>
                             <label htmlFor="answer">Answer</label>
-                            <textarea className="form-control" name="answer" placeholder="Answer" />
+                            <textarea className="form-control" name="answer" placeholder="Answer"
+                                value={answer} onChange={this.handleChange} />
+                            {submitted && !answer &&
+                                <div className="help-block">Answer is required.</div>
+                            }
                         </div>
                         <div className="form-check">
-                            <input type="checkbox" className="form-check-input" name="isActive" />
+                            <input type="checkbox" className="form-check-input" name="isActive" value={isActive} onClick={this.handleChange} />
                             <label className="form-check-label" htmlFor="isActive">is Active</label>
                         </div>
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-primary btn-sm">Save changes</button>
+                        <button type="submit" className="btn btn-primary btn-sm">Save changes</button>
                         <button type="button" className="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
                     </div>
                 </form>
