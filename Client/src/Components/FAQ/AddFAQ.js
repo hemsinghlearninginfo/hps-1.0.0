f4137aad02672300ada6104328be4e5d16f40fbf
Hem Singh <hemsingh@publicisgroupe.net>
2019-07-21 13:28:47 +0530

    window size for addFaq

--- Client/src/Components/FAQ/AddFAQ.js
+++ Client/src/Components/FAQ/AddFAQ.js
@@ -6,27 +6,28 @@ class AddFAQ extends React.Component {
         super(props);
     }
 
-    saveFAQ(){
-        console.log('save Changes from child');
-    }
-
     render() {
         return (
             <>
                 <form>
-                    <div className="form-group">
-                        <label htmlFor="question">Question</label>
-                        <input type="type" className="form-control" name="question" placeholder="Question" />
+                    <div className="modal-body">
+                        <div className="form-group">
+                            <label htmlFor="question">Question</label>
+                            <input type="type" className="form-control" name="question" placeholder="Question" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="answer">Answer</label>
+                            <textarea className="form-control" name="answer" placeholder="Answer" />
+                        </div>
+                        <div className="form-check">
+                            <input type="checkbox" className="form-check-input" name="isActive" />
+                            <label className="form-check-label" htmlFor="isActive">is Active</label>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="answer">Answer</label>
-                        <textarea  className="form-control" name="answer" placeholder="Answer" />
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-primary btn-sm">Save changes</button>
+                        <button type="button" className="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
                     </div>
-                    <div className="form-check">
-                        <input type="checkbox" className="form-check-input" name="isActive" />
-                        <label className="form-check-label" htmlFor="isActive">is Active</label>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </>
         );
--- Client/src/_controls/ModalPopUp.js
+++ Client/src/_controls/ModalPopUp.js
@@ -2,12 +2,13 @@ import React from 'react';
 export const ModalPopUp = (props) => (
     <>
         <button type="button" className="btn btn-primary btn-sm" data-toggle="modal"
+        data-backdrop="static" data-keyboard="false"
             data-target="#modalPopUp">
             {props.btnLabel}
         </button>
         <div className="modal fade" id="modalPopUp" tabIndex="-1" role="dialog"
             aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog" role="document">
+            <div className="modal-dialog modal-lg" role="document">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title" id="exampleModalLabel">{props.heading}</h5>
@@ -15,13 +16,7 @@ export const ModalPopUp = (props) => (
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div className="modal-body">
-                        {props.children}
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-primary btn-sm" onClick={props.saveChanges}>Save changes</button>
-                        <button type="button" className="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
-                    </div>
+                    {props.children}
                 </div>
             </div>
         </div>
